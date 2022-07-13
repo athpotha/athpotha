31d0ef2caa395cf1772a57986b0a8fd61551af1c
Kumudperera <nethmalperera54@gmail.com>
2022-07-13 21:20:15 +0530

    Fotter added

--- athpotha/athpothaFrontEnd/src/components/pages/home/Home.js
+++ athpotha/athpothaFrontEnd/src/components/pages/home/Home.js
@@ -6,17 +6,19 @@ import TopRatedTutors from "./TopRatedTutors";
 import { Stack } from "@mui/material";
 import UniversityDegrees from "./UniversityDegrees";
 import CommiunityExperts from "./CommiunityExperts";
+import Footer from "./Footer";
 
 function Home() {
   return (
     <StyledEngineProvider injectFirst>
       <HomeAppBar />
-      <Stack direction="column" spacing={8}>
+      <Stack direction="column" spacing={8}  style={{margin: "0 0 50px 0"}}>
         <StudentLogin />
         <TopRatedTutors />
         <UniversityDegrees />
         <CommiunityExperts />
       </Stack>
+      <Footer />
     </StyledEngineProvider>
   );
 }
