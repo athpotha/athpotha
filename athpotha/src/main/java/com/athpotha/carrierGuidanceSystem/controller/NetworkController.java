b7e0e768dabaa8fae4ceb30a635d417c23ace379
AshaniImalsha <ashaniimalsha26@gmail.com>
2022-08-19 12:57:38 +0530

    network controller

--- athpotha/src/main/java/com/athpotha/carrierGuidanceSystem/controller/NetworkController.java
+++ athpotha/src/main/java/com/athpotha/carrierGuidanceSystem/controller/NetworkController.java
@@ -1,2 +1,37 @@
-package com.athpotha.carrierGuidanceSystem.controller;public class NetworkController {
+package com.athpotha.carrierGuidanceSystem.controller;
+
+import com.athpotha.carrierGuidanceSystem.model.*;
+import com.athpotha.carrierGuidanceSystem.repository.TutorRepository;
+import com.athpotha.carrierGuidanceSystem.repository.UserRepository;
+import com.athpotha.carrierGuidanceSystem.service.UserService;
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin
+@RestController
+@RequestMapping("network")
+public class NetworkController {
+
+    @Autowired
+    private TutorRepository tutorRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("getTutors")
+    public List<Tutor> getTutors(@RequestBody User user){
+        Long id = 1L;
+        List<Tutor> tutorsa  = tutorRepository.findTutorsunFollowing(id);
+        return tutorsa;
+    }
+
+
 }
