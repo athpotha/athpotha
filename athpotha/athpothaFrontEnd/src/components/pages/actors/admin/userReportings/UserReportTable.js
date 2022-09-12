02021d50d958f3a591b80f95962e833490162418
Roneki Saranga Manamperi <48724352+Roneki@users.noreply.github.com>
2022-09-12 12:39:50 +0530

    file structure changed

--- athpotha/athpothaFrontEnd/src/components/pages/actors/admin/userReportings/UserReportTable.js
+++ athpotha/athpothaFrontEnd/src/components/pages/actors/admin/userReportings/UserReportTable.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-<<<<<<< HEAD
 import PropTypes from "prop-types";
 import {
   DataGrid,
@@ -10,17 +9,6 @@ import {
 // import styled from "@emotion/styled";
 import { green, red, blue, orange } from "@mui/material/colors";
 import CenteredBox from "../../../../ui/CenteredBox";
-=======
-import { styled } from "@mui/material/styles";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell, { tableCellClasses } from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import { create } from "@mui/material/styles/createTransitions";
->>>>>>> fdd58da438b8c85638e2dd4fb4b6fdeb9d9b0e98
 import {
   Box,
   Button,
@@ -30,7 +18,6 @@ import {
   MenuItem,
   Select,
 } from "@mui/material";
-<<<<<<< HEAD
 import { alpha, styled } from "@mui/material/styles";
 
 //Filter panel
@@ -48,55 +35,6 @@ CustomToolbar.propTypes = {
 const ColorButton1 = styled(Button)(({ theme }) => ({
   color: theme.palette.getContrastText(blue[600]),
   textTransform: "none",
-=======
-import { FormLabel } from "@mui/material";
-import {
-  blue,
-  blueGrey,
-  green,
-  orange,
-  red,
-  yellow,
-} from "@mui/material/colors";
-
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  "&:nth-of-type(odd)": {
-    backgroundColor: theme.palette.action.hover,
-  },
-  // hide last border
-  "&:last-child td, &:last-child th": {
-    border: 0,
-  },
-}));
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("1", "2", "2022-01-5", "Fake Account"),
-  createData("5", "8", "2022-01-6", "Malicious COntent"),
-  createData("10", "3", "2022-01-7", "Malicious COntent"),
-  createData("5", "7", "2022-01-8", "Malicious COntent"),
-  createData("9", "11", "2022-01-9", "Fake Account"),
-];
-const updateRow = () => {
-  console.log("user reports");
-};
-
-const ColorButton1 = styled(Button)(({ theme }) => ({
-  color: theme.palette.getContrastText(blue[600]),
->>>>>>> fdd58da438b8c85638e2dd4fb4b6fdeb9d9b0e98
   backgroundColor: blue[600],
   "&:hover": {
     backgroundColor: blue[700],
@@ -105,10 +43,7 @@ const ColorButton1 = styled(Button)(({ theme }) => ({
 
 const ColorButton2 = styled(Button)(({ theme }) => ({
   color: theme.palette.getContrastText(green[600]),
-<<<<<<< HEAD
   textTransform: "none",
-=======
->>>>>>> fdd58da438b8c85638e2dd4fb4b6fdeb9d9b0e98
   backgroundColor: green[600],
   "&:hover": {
     backgroundColor: green[700],
@@ -118,30 +53,21 @@ const ColorButton2 = styled(Button)(({ theme }) => ({
 const ColorButton3 = styled(Button)(({ theme }) => ({
   color: theme.palette.getContrastText(red[600]),
   backgroundColor: red[600],
-<<<<<<< HEAD
   textTransform: "none",
-=======
->>>>>>> fdd58da438b8c85638e2dd4fb4b6fdeb9d9b0e98
   "&:hover": {
     backgroundColor: red[700],
   },
 }));
 
 const ColorButton4 = styled(Button)(({ theme }) => ({
-<<<<<<< HEAD
   color: theme.palette.getContrastText(orange[600]),
   backgroundColor: orange[600],
   textTransform: "none",
-=======
-  color: theme.palette.getContrastText(blueGrey[600]),
-  backgroundColor: orange[600],
->>>>>>> fdd58da438b8c85638e2dd4fb4b6fdeb9d9b0e98
   "&:hover": {
     backgroundColor: orange[700],
   },
 }));
 
-<<<<<<< HEAD
 const rows = [
   {
     id: 1,
@@ -236,15 +162,11 @@ const style = {
   backgroundColor:"white"
 };
 export default function ManageUSerTable() {
-=======
-export default function CustomizedTables() {
->>>>>>> fdd58da438b8c85638e2dd4fb4b6fdeb9d9b0e98
   const [age, setAge] = React.useState("");
 
   const handleChange = (event) => {
     setAge(event.target.value);
   };
-<<<<<<< HEAD
   const [filterButtonEl, setFilterButtonEl] = React.useState(null);
   return (
     <Grid>
@@ -273,77 +195,6 @@ export default function CustomizedTables() {
           />
         </Box>
       </Box>
-=======
-  return (
-    <Grid>
-      {/* <Grid container>
-      
-      <Grid item xs={12}>
-      <Box sx={{ maxWidth: 200 ,mb:3}}>
-        <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">Select User Type</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={age}
-            label="User_Type"
-            onChange={handleChange}
-          >
-            <MenuItem value={10}>Student</MenuItem>
-            <MenuItem value={20}>Teacher</MenuItem>
-            <MenuItem value={30}>University Representative</MenuItem>
-            <MenuItem value={40}>Stakeholders</MenuItem>
-
-          </Select>
-        </FormControl>
-      </Box>
-      </Grid>
-      </Grid> */}
-
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth:700 }} aria-label="customized table">
-          <TableHead>
-            <TableRow>
-              <StyledTableCell align="left">Reporter Id</StyledTableCell>
-              <StyledTableCell align="left">Reportee Id</StyledTableCell>
-              <StyledTableCell align="left">Date</StyledTableCell>
-              <StyledTableCell align="left">Reason</StyledTableCell>
-              <StyledTableCell align="left"></StyledTableCell>
-              <StyledTableCell align="center">Actions</StyledTableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <StyledTableRow key={row.name}>
-                <StyledTableCell component="th" scope="row">
-                  {row.name}
-                </StyledTableCell>
-                <StyledTableCell align="left">{row.calories}</StyledTableCell>
-                <StyledTableCell align="left">{row.fat}</StyledTableCell>
-                <StyledTableCell align="left">{row.carbs}</StyledTableCell>
-                <StyledTableCell align="left">{row.protein}</StyledTableCell>
-                <StyledTableCell align="left">
-                  <Grid container spacing={3}>
-                    <Grid item>
-                      <ColorButton1 variant="contained">View User</ColorButton1>
-                    </Grid>
-                    <Grid item>
-                      <ColorButton2 variant="contained">Reports</ColorButton2>
-                    </Grid>
-                    <Grid item>
-                      <ColorButton3 variant="contained">Block</ColorButton3>
-                    </Grid>
-                    <Grid item>
-                      <ColorButton4 variant="contained">Warn</ColorButton4>
-                    </Grid>
-                  </Grid>
-                </StyledTableCell>
-              </StyledTableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
->>>>>>> fdd58da438b8c85638e2dd4fb4b6fdeb9d9b0e98
     </Grid>
   );
 }
--- athpotha/athpothaFrontEnd/src/components/pages/profile/CoverSection.js
+++ athpotha/athpothaFrontEnd/src/components/pages/profile/CoverSection.js
@@ -1,4 +1,4 @@
-import { Grid, InputLabel, MenuItem, Select, StyledEngineProvider } from "@mui/material";
+import { Grid, StyledEngineProvider } from "@mui/material";
 import React from 'react';
 import { Box, Container } from "@mui/system";
 import { useNavigate } from "react-router-dom";
@@ -22,9 +22,6 @@ import SettingsIcon from '@mui/icons-material/Settings';
 import ChangePassword from "./ChangePassword"
 import styled from "@emotion/styled";
 import ChangeImage from "./ChangeImage";
-import { fetchUserData } from "../../../api/authenticationService";
-
-
 
 function CoverSection(props) {
   const style = {
@@ -40,10 +37,8 @@ function CoverSection(props) {
     width: "58%",
 
   };
-  
-const editInfo =(e)=>{
-  e.preventDeafult();
-}
+
+
   // ------------- Model -----------
 
   const [openOne, setOpenOne] = React.useState(false);
@@ -53,12 +48,7 @@ const editInfo =(e)=>{
   const [openTwo, setOpenTwo] = React.useState(false);
   const handleOpenTwo = () => setOpenTwo(true);
   const handleCloseTwo = () => setOpenTwo(false);
-  
-  const [firstName,setFirstName]= React.useState( localStorage.getItem("FIRST_NAME"));
-  const [lastName,setLastName]= React.useState(localStorage.getItem("LAST_NAME"));
-  const [description,setDescription]= React.useState(localStorage.getItem("DESCRIPTION"));
-  const[studentType,setStudentType]=React.useState(localStorage.getItem("STUDENT_TYPE"));
-  // console.log("studentType"+studentType);
+
   let subText = ""
   switch (localStorage.getItem("USER_TYPE")) {
     case "student":
@@ -77,49 +67,12 @@ const editInfo =(e)=>{
     // maxWidth: 330,
   });
   // ----------------------------------
-  var id=localStorage.getItem("USER_ID");
-
-  const handleChange=(e)=>{
-    console.log(e);
-  }
-
-  const handleSubmit=(e)=>{
-    e.preventDefault();
-    console.log(id) ;
-    // var dataSet=JSON.stringify(data);
-    const data = {
-      url: `profile/edit-info/${id}`,
-      method: "put",
-      data: {
-        firstName,
-        lastName,
-        description
-      },
-    };
-    fetchUserData(data)
-      .then((response) => {
-        console.log(response.data);
-        // setReadUnread(1);
-
-        // setNotiDBData(response.data);
-        localStorage.setItem("FIRST_NAME",firstName);
-        localStorage.setItem("LAST_NAME",lastName);
-        localStorage.setItem("DESCRIPTION",description);
-
-        window.location.replace("/profile");
-
-      })
-      .catch((e) => {
-        console.log("CATCH---------");
-        console.log(e);
-      });
-    // console.log(data);
-  }
-
   return (
     <StyledEngineProvider injectFirst>
       <div sx={{ width: "100%" }}>
-        <Box sx={{ bgcolor: "background", mb: 3, borderRadius: 2 }}>
+        <Box
+          sx={{ bgcolor: "background", mb: 3, borderRadius: 2 }}
+        >
           <Box>
             <Card sx={{ maxWidth: "100%" }}>
               <div style={{ position: "relative" }}>
@@ -137,23 +90,32 @@ const editInfo =(e)=>{
                       <PhotoCamera />
                     </IconButton>
                   </ChangeImage>
+
                 </div>
                 {/* ----------------------------- White section ------------------------------- */}
                 <ChangeImage tabValue={0}>
                   <IconButton
                     style={{ position: "absolute", top: "130px", left: "16px" }}
                   >
-                    <ProfileAvatar src={localStorage.getItem("PROFILE_PIC")} />
+                    <ProfileAvatar
+                      src={localStorage.getItem("PROFILE_PIC")}
+                    />
                   </IconButton>
                 </ChangeImage>
 
+
                 {/* --------------------------------- Model Start ----------------------------- */}
+
 
                 <Grid>
                   <Modal open={openOne}>
                     <Box sx={style}>
                       <Grid>
-                        <Typography variant="h5" component="h2" color="#1e88e5">
+                        <Typography
+                          variant="h5"
+                          component="h2"
+                          color="#1e88e5"
+                        >
                           EDIT INFO
                         </Typography>
                       </Grid>
@@ -174,18 +136,13 @@ const editInfo =(e)=>{
                       >
                         {/* ---------------------content of the model start ------------------ */}
 
-                        <form
-                          style={{ maxWidth: "100%" }}
-                          onSubmit={handleSubmit}
-                        >
+                        <form style={{ maxWidth: "100%" }}>
                           <TextField
                             id="First Name"
                             label="First Name"
                             variant="outlined"
-                            fullWidth
-                            onChange={(e) => setFirstName(e.target.value)} //
-                            value={firstName}
                             defaultValue={localStorage.getItem("FIRST_NAME")}
+                            fullWidth
                           />
 
                           <TextField
@@ -193,45 +150,16 @@ const editInfo =(e)=>{
                             label="Last Name"
                             variant="outlined"
                             defaultValue={localStorage.getItem("LAST_NAME")}
-                            onChange={(e) => setLastName(e.target.value)}
-                            value={lastName}
                           />
                           <TextField
                             id="Bio"
                             label="Bio"
                             multiline
-                            rows={1}
-                            defaultValue={localStorage.getItem("DESCRIPTION")}
-                            onChange={(e) => setDescription(e.target.value)}
-                            value={description}
+                            rows={4}
+                            defaultValue="Default Value"
                           />
+                          <Button variant="contained">Save</Button>
 
-
-                          {/* select dropdown */}
-                          {/* <InputLabel id="demo-simple-select-label">
-                            Student Type
-                          </InputLabel> */}
-                          <Select
-                            labelId="demo-simple-select-label"
-                            id="Student_Type"
-                            value={studentType}
-                            label="Student Type"
-                            // defaultValue={studentType}
-                            onChange={(e)=>handleChange(e.target.value)}
-                          >
-                            <MenuItem value={"Regular"}>Regular</MenuItem>
-                            <MenuItem value={"OL_Qualified"}>OL_Qualified</MenuItem>
-                            <MenuItem value={30}>AL_Qualified</MenuItem>
-                            <MenuItem value={40}>Undergraduate</MenuItem>
-
-                          </Select>
-
-
-
-                          <Button variant="contained" type="submit">
-                            Save
-                          </Button>
-                          {/* <input type="submit"/> */}
                           {/* ---------------------content of the model end ------------------ */}
                         </form>
                       </Box>
@@ -250,8 +178,7 @@ const editInfo =(e)=>{
                     }}
                   >
                     <Box sx={{ fontWeight: "bold" }}>
-                      {localStorage.getItem("FIRST_NAME")}{" "}
-                      {localStorage.getItem("LAST_NAME")}
+                      {localStorage.getItem("USER_NAME")}
                     </Box>
                     <Box sx={{ fontSize: "10pt" }}>{subText}</Box>
                     <Box sx={{ fontSize: "10pt" }}>
@@ -285,12 +212,17 @@ const editInfo =(e)=>{
           </Box>
         </Box>
 
+
         {/* <---------------- Settings Pop Up ---------------------> */}
         <Grid>
           <Modal open={openTwo}>
             <Box sx={style}>
               <Grid>
-                <Typography variant="h5" component="h2" color="#1e88e5">
+                <Typography
+                  variant="h5"
+                  component="h2"
+                  color="#1e88e5"
+                >
                   Settings
                 </Typography>
               </Grid>
@@ -306,13 +238,16 @@ const editInfo =(e)=>{
               >
                 <CloseIcon />
               </IconButton>
-              <Box sx={{ mt: "2px", height: "100%", overflow: "auto" }}>
+              <Box
+                sx={{ mt: "2px", height: "100%", overflow: "auto" }}
+              >
                 {/* ---------------------content of the model start ------------------ */}
 
-                <form style={{ maxWidth: "100%" }}>
-                  {" "}
-                  {/*  FORM HERE ???? */}
+                <form style={{ maxWidth: "100%" }}>  {/*  FORM HERE ???? */}
+
+
                   <ChangePassword></ChangePassword>
+
                   {/* ---------------------content of the model end ------------------ */}
                 </form>
               </Box>
@@ -322,7 +257,12 @@ const editInfo =(e)=>{
       </div>
 
       {/* -------------------------------------- settings Pop up End   ------------------------------- */}
+
+
+
+
     </StyledEngineProvider>
+
   );
 }
 
